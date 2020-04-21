482555ecc3654a58276903d9fe4b53f651361fe4
Unnati Mishra <54355445+CodesbyUnnati@users.noreply.github.com>
2020-04-21 23:21:02 +0530

    Create Simple_Input.cpp

---
+++ C++ Language/Simple_Input.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+  int n;
+  cin>>n;
+  int sum=0;
+  while(sum+n>=0)
+   {
+      sum+n;
+      cout<<n<<endl;
+      cin>>n;
+   }
+ return0;
+ }
